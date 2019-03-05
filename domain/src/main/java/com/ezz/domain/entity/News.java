067f5eb4df368e48041c3b0010d828b7c57bb1ec
Ezz Ben Walid <ewalid@icon-creations.com>
2019-03-05 12:55:04 +0200

    added news entity

---
+++ domain/src/main/java/com/ezz/domain/entity/News.java
@@ -0,0 +1,87 @@
+package com.ezz.domain.entity;
+
+/**
+ * Created by Ezz Waleed on 04,March,2019
+ */
+public class News {
+
+    private String title;
+
+    private String description;
+
+    private String content;
+
+    private String url;
+
+    private String imageUrl;
+
+    private String publishedDate;
+
+    private String authorName;
+
+    private String sourceName;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public void setSourceName(String sourceName) {
+        this.sourceName = sourceName;
+    }
+}
