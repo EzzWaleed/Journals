5cd1dd164d9c102d2dc2428b04a723f3e29b8049
Ezz Ben Walid <ewalid@icon-creations.com>
2019-03-08 00:59:48 +0200

    support java 8

--- ui/src/main/java/com/ezz/newsapp/NewsActivity.java
+++ ui/src/main/java/com/ezz/newsapp/NewsActivity.java
@@ -2,14 +2,11 @@ package com.ezz.newsapp;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import butterknife.BindView;
 
-import android.view.View;
 
 public class NewsActivity extends AppCompatActivity {
 
