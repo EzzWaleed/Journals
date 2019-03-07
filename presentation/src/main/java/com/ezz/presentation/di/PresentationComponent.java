1c8f803bb01bc9cbc11a4cf6812e2b32282e61cd
Ezz Ben Walid <ewalid@icon-creations.com>
2019-03-08 00:16:41 +0200

    make  presentation component depend on data component

--- presentation/src/main/java/com/ezz/presentation/di/PresentationComponent.java
+++ presentation/src/main/java/com/ezz/presentation/di/PresentationComponent.java
@@ -1,5 +1,6 @@
 package com.ezz.presentation.di;
 
+import com.ezz.data.di.DataComponent;
 import com.ezz.presentation.mapper.di.MapperModule;
 import com.ezz.presentation.viewmodel.viewmodel_factory.ViewModelFactory;
 import com.ezz.presentation.viewmodel.viewmodel_factory.di.ViewModelFactoryModule;
@@ -9,9 +10,9 @@ import dagger.Component;
 /**
  * Created by Ezz Waleed on 07,March,2019
  */
-@Component(modules = {MapperModule.class, ViewModelFactoryModule.class})
+@Component(modules = {MapperModule.class, ViewModelFactoryModule.class}, dependencies = DataComponent.class)
 @PresentationScope
-public abstract class PresentationComponent {
-	ViewModelFactory viewModelFactory;
-	MapperModule mapperModule;
+public interface  PresentationComponent {
+	ViewModelFactory viewModelFactory();
+	MapperModule mapperModule();
 }
---
+++ ui/src/main/java/com/ezz/newsapp/App.java
@@ -0,0 +1,14 @@
+package com.ezz.newsapp;
+
+import android.app.Application;
+
+/**
+ * Created by Ezz Waleed on 07,March,2019
+ */
+public class App extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+	}
+}
