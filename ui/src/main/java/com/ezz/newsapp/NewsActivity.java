71f43d9173c556cf04c5357cecbf48f35d95fbe9
Ezz Ben Walid <ewalid@icon-creations.com>
2019-03-07 20:58:35 +0200

    added news_list_item

--- ui/src/main/java/com/ezz/newsapp/NewsActivity.java
+++ ui/src/main/java/com/ezz/newsapp/NewsActivity.java
@@ -19,13 +19,5 @@ public class NewsActivity extends AppCompatActivity {
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-				.setAction("Action", null).show();
-			}
-		});
 	}
 }
