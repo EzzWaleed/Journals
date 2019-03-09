cdab29ab8ff0b2990b866f01805c73083e8c972a
Ezz Ben Walid <ewalid@icon-creations.com>
2019-03-09 02:38:08 +0200

    added search view logic

--- ui/src/main/java/com/ezz/newsapp/news/NewsActivity.java
+++ ui/src/main/java/com/ezz/newsapp/news/NewsActivity.java
@@ -7,7 +7,9 @@ import com.ezz.newsapp.R;
 import com.ezz.newsapp.news.adapter.NewsAdapter;
 import com.ezz.newsapp.news.di.DaggerNewsScreenComponent;
 import com.ezz.newsapp.paging.PagingManger;
+import com.ezz.presentation.model.NewsUI;
 import com.ezz.presentation.viewmodel.news.NewsViewModel;
+import com.ezz.presentation.viewmodel.search.SearchViewModel;
 import com.ezz.presentation.viewmodel.viewmodel_factory.ViewModelFactory;
 import com.google.android.material.snackbar.Snackbar;
 import com.paginate.Paginate;
@@ -15,14 +17,17 @@ import com.paginate.Paginate;
 import javax.inject.Inject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.paging.PagedList;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class NewsActivity extends AppCompatActivity implements PagingManger.LoadMoreListener{
+public class NewsActivity extends AppCompatActivity implements PagingManger.LoadMoreListener, SearchView.OnQueryTextListener, SearchView.OnCloseListener{
 
 	@Inject
 	NewsAdapter newsAdapter;
@@ -37,8 +42,13 @@ public class NewsActivity extends AppCompatActivity implements PagingManger.Load
 	Toolbar toolbar;
 	@BindView(R.id.news_recycler_view)
 	RecyclerView recyclerView;
+	@BindView(R.id.search_view)
+	SearchView searchView;
 
 	NewsViewModel newsViewModel;
+	SearchViewModel searchViewModel;
+
+	private Paginate paginate;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +62,11 @@ public class NewsActivity extends AppCompatActivity implements PagingManger.Load
 		.build().inject(this);
 
 		newsViewModel = ViewModelProviders.of(this, viewModelFactory).get(NewsViewModel.class);
+		searchViewModel = ViewModelProviders.of(this, viewModelFactory).get(SearchViewModel.class);
 
-		newsViewModel.newsPagedListLiveData.observe(this, newsUIS -> newsAdapter.submitList(newsUIS));
+		searchView.setOnQueryTextListener(this);
+
+		newsViewModel.newsPagedListLiveData.observe(this, pagedListObserver);
 
 		newsViewModel.loadNewsStats.observe(this, dataStatus -> {
 			switch (dataStatus){
@@ -74,10 +87,15 @@ public class NewsActivity extends AppCompatActivity implements PagingManger.Load
 			pagingManger.setLoading(false);
 		});
 
+
 		recyclerView.setAdapter(newsAdapter);
 		recyclerView.setLayoutManager(new LinearLayoutManager(this));
-		Paginate.with(recyclerView, pagingManger).addLoadingListItem(false).setLoadingTriggerThreshold(50).build();
+		paginate = Paginate.with(recyclerView, pagingManger).addLoadingListItem(false).setLoadingTriggerThreshold(50).build();
 	}
+
+	Observer<PagedList<NewsUI>> pagedListObserver = newsUIPagedList -> {
+		newsAdapter.submitList(newsUIPagedList);
+	};
 
 	@Override
 	public void onLoadMore() {
@@ -85,4 +103,24 @@ public class NewsActivity extends AppCompatActivity implements PagingManger.Load
 		newsViewModel.loadNews(pagingManger.getPageNumber());
 	}
 
+	@Override
+	public boolean onQueryTextSubmit(String query) {
+		paginate.unbind();
+		newsViewModel.newsPagedListLiveData.removeObserver(pagedListObserver);
+		searchViewModel.newsLiveData.observe(this, pagedListObserver);
+		return true;
+	}
+
+	@Override
+	public boolean onQueryTextChange(String newText) {
+		return false;
+	}
+
+	@Override
+	public boolean onClose() {
+		searchViewModel.newsLiveData.removeObserver(pagedListObserver);
+		newsViewModel.newsPagedListLiveData.observe(this, pagedListObserver);
+		paginate = Paginate.with(recyclerView, pagingManger).addLoadingListItem(false).setLoadingTriggerThreshold(50).build();
+		return true;
+	}
 }
