1f969e0582f99cfbb22f5564b4056b1255c63e83
Ezz Ben Walid <ewalid@icon-creations.com>
2019-03-09 07:01:45 +0200

    adding newsdetails design

---
+++ ui/src/main/java/com/ezz/newsapp/news/details/DetailsActivity.java
@@ -0,0 +1,21 @@
+package com.ezz.newsapp.news.details;
+
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import com.ezz.newsapp.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
